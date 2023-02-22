7ab3c3f6a8c3bfcc9c797d373e1ff7d89b27bb15
Dmitriy Korniychuk <dmitriy@korniychuk.org.ua>
2023-02-22 21:54:33 +0200

    Set specific icon URL for Cosanta and PirateCash

--- UnstoppableWallet/UnstoppableWallet/Extensions/Coin.swift
+++ UnstoppableWallet/UnstoppableWallet/Extensions/Coin.swift
@@ -4,8 +4,15 @@ import MarketKit
 extension Coin {
 
     var imageUrl: String {
-        let scale = Int(UIScreen.main.scale)
-        return "https://cdn.blocksdecoded.com/coin-icons/32px/\(uid)@\(scale)x.png"
+        switch uid{
+        case "piratecash":
+            return "https://p.cash/logo.png"
+        case "cosanta":
+            return "https://cosanta.net/logo.png"
+        default:
+            let scale = Int(UIScreen.main.scale)
+            return "https://cdn.blocksdecoded.com/coin-icons/32px/\(uid)@\(scale)x.png"
+        }
     }
 
 }
