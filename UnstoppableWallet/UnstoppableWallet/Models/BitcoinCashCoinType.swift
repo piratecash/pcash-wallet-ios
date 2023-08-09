af4dc6808c0924d265a9d09af911edd403454e07
ant013 <ant013@mail.ru>
2023-08-09 16:45:08 +0600

    Refactor token types to use it's instead of CoinSettingTypes

--- UnstoppableWallet/UnstoppableWallet/Models/BitcoinCashCoinType.swift
+++ UnstoppableWallet/UnstoppableWallet/Models/BitcoinCashCoinType.swift
@@ -1,3 +1,5 @@
+import MarketKit
+
 enum BitcoinCashCoinType: String, CaseIterable {
     case type0
     case type145
@@ -24,6 +26,17 @@ enum BitcoinCashCoinType: String, CaseIterable {
         switch self {
         case .type145: return 0
         case .type0: return 1
+        }
+    }
+
+}
+
+extension TokenType.AddressType {
+
+    var bitcoinCashCoinType: BitcoinCashCoinType {
+        switch self {
+        case .type0: return .type0
+        case .type145: return .type145
         }
     }
 
