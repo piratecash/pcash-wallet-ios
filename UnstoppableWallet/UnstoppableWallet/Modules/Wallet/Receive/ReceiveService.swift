aa6d08a8af44407294b236952fb39f8e988e016a
ant013 <ant013@mail.ru>
2023-09-12 16:26:19 +0600

    Change description for receive - change icon for "Contact Us"

--- UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/ReceiveService.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Wallet/Receive/ReceiveService.swift
@@ -144,6 +144,10 @@ extension ReceiveService {
 }
 extension ReceiveService {
 
+    func isEnabled(coin: Coin) -> Bool {
+        walletManager.activeWallets.contains { $0.coin == coin }
+    }
+
     var predefinedCoins: [FullCoin] {
         // get all restored coins
         let activeWallets = walletManager.activeWallets
@@ -162,9 +166,18 @@ extension ReceiveService {
         // filter not supported by current account
         let predefined = fullCoins?.filter { fullCoin in
             fullCoin.tokens.contains { account.type.supports(token: $0) }
-        } ?? []
+        }.sorted { lhsCoin, rhsCoin in
+            let lhsEnabled = isEnabled(coin: lhsCoin.coin)
+            let rhsEnabled = isEnabled(coin: rhsCoin.coin)
 
-        return predefined
+            if lhsEnabled != rhsEnabled {
+                return lhsEnabled
+            }
+
+            return lhsCoin.coin.marketCapRank ?? Int.max < rhsCoin.coin.marketCapRank ?? Int.max
+        }
+
+        return predefined ?? []
     }
 
 }
