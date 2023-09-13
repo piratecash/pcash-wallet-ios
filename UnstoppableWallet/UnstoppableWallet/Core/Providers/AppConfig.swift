horizontalsystems/unstoppable-wallet-ios

Extract app label for queues into AppConfig

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 struct AppConfig {
+    static let label = "io.horizontalsystems.unstoppable"
+
     static let reportEmail = "support.unstoppable@protonmail.com"
     static let companyWebPageLink = "https://horizontalsystems.io"
     static let appWebPageLink = "https://unstoppable.money"

piratecash/pcash-wallet-ios

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -1,6 +1,8 @@
 import Foundation
 
 struct AppConfig {
+    static let label = "cash.p.terminal"
+
     static let reportEmail = "i@p.cash"
     static let companyWebPageLink = "https://p.cash/"
     static let appWebPageLink = "https://p.cash/"