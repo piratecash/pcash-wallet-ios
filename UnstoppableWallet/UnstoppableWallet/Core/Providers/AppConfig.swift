horizontalsystems/unstoppable-wallet-ios

Replace `AppConfigProvider` with static `AppConfig`

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -1,114 +1,108 @@
 import Foundation
 
-class AppConfigProvider {
+struct AppConfig {
     static let reportEmail = "support.unstoppable@protonmail.com"
+    static let companyWebPageLink = "https://horizontalsystems.io"
+    static let appWebPageLink = "https://unstoppable.money"
+    static let analyticsLink = "https://unstoppable.money/analytics"
+    static let appGitHubLink = "https://github.com/horizontalsystems/unstoppable-wallet-ios"
+    static let appTwitterAccount = "unstoppablebyhs"
+    static let btcCoreRpcUrl = "https://btc.blocksdecoded.com/rpc"
+    static let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!
+    static let faqIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/unstoppable-wallet-website/master/src/faq.json")!
 
-    let companyWebPageLink = "https://horizontalsystems.io"
-    let appWebPageLink = "https://unstoppable.money"
-    let analyticsLink = "https://unstoppable.money/analytics"
-    let appGitHubLink = "https://github.com/horizontalsystems/unstoppable-wallet-ios"
-    let appTwitterAccount = "unstoppablebyhs"
-    let reportEmail = AppConfigProvider.reportEmail
-    let btcCoreRpcUrl = "https://btc.blocksdecoded.com/rpc"
-    let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!
-    let faqIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/unstoppable-wallet-website/master/src/faq.json")!
-
-    var marketApiUrl: String {
+    static var marketApiUrl: String {
         (Bundle.main.object(forInfoDictionaryKey: "MarketApiUrl") as? String) ?? ""
     }
 
-    var officeMode: Bool {
+    static var officeMode: Bool {
         Bundle.main.object(forInfoDictionaryKey: "OfficeMode") as? String == "true"
     }
 
-    var infuraCredentials: (id: String, secret: String?) {
+    static var infuraCredentials: (id: String, secret: String?) {
         let id = (Bundle.main.object(forInfoDictionaryKey: "InfuraProjectId") as? String) ?? ""
         let secret = Bundle.main.object(forInfoDictionaryKey: "InfuraProjectSecret") as? String
         return (id: id, secret: secret)
     }
 
-    var etherscanKey: String {
+    static var etherscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "EtherscanApiKey") as? String) ?? ""
     }
 
-    var arbiscanKey: String {
+    static var arbiscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "ArbiscanApiKey") as? String) ?? ""
     }
 
-    var gnosisscanKey: String {
+    static var gnosisscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "GnosisscanApiKey") as? String) ?? ""
     }
 
-    var ftmscanKey: String {
+    static var ftmscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "FtmscanApiKey") as? String) ?? ""
     }
 
-    var optimismEtherscanKey: String {
+    static var optimismEtherscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "OptimismEtherscanApiKey") as? String) ?? ""
     }
 
-    var bscscanKey: String {
+    static var bscscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "BscscanApiKey") as? String) ?? ""
     }
 
-    var polygonscanKey: String {
+    static var polygonscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "PolygonscanApiKey") as? String) ?? ""
     }
 
-    var snowtraceKey: String {
+    static var snowtraceKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "SnowtraceApiKey") as? String) ?? ""
     }
 
-    var cryptoCompareApiKey: String? {
+    static var cryptoCompareApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "CryptoCompareApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var defiYieldApiKey: String? {
+    static var defiYieldApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "DefiYieldApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var twitterBearerToken: String? {
+    static var twitterBearerToken: String? {
         (Bundle.main.object(forInfoDictionaryKey: "TwitterBearerToken") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var hsProviderApiKey: String? {
+    static var hsProviderApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "HsProviderApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var tronGridApiKey: String? {
+    static var tronGridApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "TronGridApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var walletConnectV2ProjectKey: String? {
+    static var walletConnectV2ProjectKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "WallectConnectV2ProjectKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var coinzixHCaptchaKey: String? {
+    static var coinzixHCaptchaKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "CoinzixHCaptchaKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var defaultWords: String {
+    static var defaultWords: String {
         Bundle.main.object(forInfoDictionaryKey: "DefaultWords") as? String ?? ""
     }
 
-    var defaultPassphrase: String {
+    static var defaultPassphrase: String {
         Bundle.main.object(forInfoDictionaryKey: "DefaultPassphrase") as? String ?? ""
     }
 
-    var sharedCloudContainer: String? {
+    static var sharedCloudContainer: String? {
         Bundle.main.object(forInfoDictionaryKey: "SharedCloudContainerId") as? String
     }
 
-    var privateCloudContainer: String? {
+    static var privateCloudContainer: String? {
         Bundle.main.object(forInfoDictionaryKey: "PrivateCloudContainerId") as? String
     }
 
-    var openSeaApiKey: String {
+    static var openSeaApiKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "OpenSeaApiKey") as? String) ?? ""
     }
 
 }
-
-
-
-

piratecash/pcash-wallet-ios

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -1,114 +1,108 @@
 import Foundation
 
-class AppConfigProvider {
+struct AppConfig {
     static let reportEmail = "i@p.cash"
+    static let companyWebPageLink = "https://p.cash/"
+    static let appWebPageLink = "https://p.cash/"
+    static let analyticsLink = "https://portfolio.cash"
+    static let appGitHubLink = "https://github.com/piratecash/pcash-wallet-ios"
+    static let appTwitterAccount = "PirateCash_NET"
+    static let btcCoreRpcUrl = "https://btc.blocksdecoded.com/rpc"
+    static let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!
+    static let faqIndexUrl = URL(string: "https://p.cash/s1/faq.json")!
 
-    let companyWebPageLink = "https://p.cash/"
-    let appWebPageLink = "https://p.cash/"
-    let analyticsLink = "https://portfolio.cash"
-    let appGitHubLink = "https://github.com/piratecash/pcash-wallet-ios"
-    let appTwitterAccount = "PirateCash_NET"
-    let reportEmail = "i@p.cash"
-    let btcCoreRpcUrl = "https://btc.blocksdecoded.com/rpc"
-    let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!
-    let faqIndexUrl = URL(string: "https://p.cash/s1/faq.json")!
-
-    var marketApiUrl: String {
+    static var marketApiUrl: String {
         (Bundle.main.object(forInfoDictionaryKey: "MarketApiUrl") as? String) ?? ""
     }
 
-    var officeMode: Bool {
+    static var officeMode: Bool {
         Bundle.main.object(forInfoDictionaryKey: "OfficeMode") as? String == "true"
     }
 
-    var infuraCredentials: (id: String, secret: String?) {
+    static var infuraCredentials: (id: String, secret: String?) {
         let id = (Bundle.main.object(forInfoDictionaryKey: "InfuraProjectId") as? String) ?? ""
         let secret = Bundle.main.object(forInfoDictionaryKey: "InfuraProjectSecret") as? String
         return (id: id, secret: secret)
     }
 
-    var etherscanKey: String {
+    static var etherscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "EtherscanApiKey") as? String) ?? ""
     }
 
-    var arbiscanKey: String {
+    static var arbiscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "ArbiscanApiKey") as? String) ?? ""
     }
 
-    var gnosisscanKey: String {
+    static var gnosisscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "GnosisscanApiKey") as? String) ?? ""
     }
 
-    var ftmscanKey: String {
+    static var ftmscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "FtmscanApiKey") as? String) ?? ""
     }
 
-    var optimismEtherscanKey: String {
+    static var optimismEtherscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "OptimismEtherscanApiKey") as? String) ?? ""
     }
 
-    var bscscanKey: String {
+    static var bscscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "BscscanApiKey") as? String) ?? ""
     }
 
-    var polygonscanKey: String {
+    static var polygonscanKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "PolygonscanApiKey") as? String) ?? ""
     }
 
-    var snowtraceKey: String {
+    static var snowtraceKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "SnowtraceApiKey") as? String) ?? ""
     }
 
-    var cryptoCompareApiKey: String? {
+    static var cryptoCompareApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "CryptoCompareApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var defiYieldApiKey: String? {
+    static var defiYieldApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "DefiYieldApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var twitterBearerToken: String? {
+    static var twitterBearerToken: String? {
         (Bundle.main.object(forInfoDictionaryKey: "TwitterBearerToken") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var hsProviderApiKey: String? {
+    static var hsProviderApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "HsProviderApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var tronGridApiKey: String? {
+    static var tronGridApiKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "TronGridApiKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var walletConnectV2ProjectKey: String? {
+    static var walletConnectV2ProjectKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "WallectConnectV2ProjectKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var coinzixHCaptchaKey: String? {
+    static var coinzixHCaptchaKey: String? {
         (Bundle.main.object(forInfoDictionaryKey: "CoinzixHCaptchaKey") as? String).flatMap { $0.isEmpty ? nil : $0 }
     }
 
-    var defaultWords: String {
+    static var defaultWords: String {
         Bundle.main.object(forInfoDictionaryKey: "DefaultWords") as? String ?? ""
     }
 
-    var defaultPassphrase: String {
+    static var defaultPassphrase: String {
         Bundle.main.object(forInfoDictionaryKey: "DefaultPassphrase") as? String ?? ""
     }
 
-    var sharedCloudContainer: String? {
+    static var sharedCloudContainer: String? {
         Bundle.main.object(forInfoDictionaryKey: "SharedCloudContainerId") as? String
     }
 
-    var privateCloudContainer: String? {
+    static var privateCloudContainer: String? {
         Bundle.main.object(forInfoDictionaryKey: "PrivateCloudContainerId") as? String
     }
 
-    var openSeaApiKey: String {
+    static var openSeaApiKey: String {
         (Bundle.main.object(forInfoDictionaryKey: "OpenSeaApiKey") as? String) ?? ""
     }
 
 }
-
-
-
-