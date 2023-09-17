horizontalsystems/unstoppable-wallet-ios

Implement donate v2 - Refactor send controllers to open its with mode (send, donate) - Refactor wallet token list to use DataSource - Implement donate datasource with description cell - Add donate adresses for all blockchains - Make @PostPublished filter variable instead of override function

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import MarketKit
 
 struct AppConfig {
     static let label = "io.horizontalsystems.unstoppable"
@@ -18,10 +19,24 @@ struct AppConfig {
     static let mempoolSpaceUrl = "https://mempool.space"
     static let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!
     static let faqIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/unstoppable-wallet-website/master/src/faq.json")!
-    static let donationAddresses: [String: String] = [
-        "BTC": "bc1qw5tw4cnyt0vxts70ntdzxesn2zzz97t6r29pjj",
-        "ETH": "0x8a2Bec907827F496752c3F24F960B3cddc5D311B",
-        "BNB": "0x8a2Bec907827F496752c3F24F960B3cddc5D311B"
+    static let donationAddresses: [BlockchainType: String] = [
+        .bitcoin: "bc1qxt5u5swx3sk6y2923whr4tvjreza43g37czv67",
+        .bitcoinCash: "bitcoincash:qz6sy9fq66yvfl5mvpfv3v2nqw5pervvkc425nj9g0\n",
+        .ecash: "ecash:qp6t4rqd4qdlq0vlucjhucjxygn5969j3cdan6ykzr\n",
+        .litecoin: "ltc1q05f90wt464h8dft9t7q9sp9n0qeprlv30070at\n",
+        .dash: "Xp24AqFUP9nF3ycLCmTDvgezxSt3RAKP2r",
+        .zcash: "zs1jpd8u7zghtq5eg48l384y6fpy7cr0xmqehnw5mujpm8v2u7jr9a3j7luftqpthf6a8f720vdfyn",
+        .ethereum: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .binanceSmartChain: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .binanceChain: "bnb1m0ys77zwg74733f5wwyzhjme2xrdq4ee84smf4",
+        .polygon: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .avalanche: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .optimism: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .arbitrumOne: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .gnosis: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .fantom: "0xA24c159C7f1E4A04dab7c364C2A8b87b3dBa4cd1",
+        .tron: "TQzANCd363w5CjRWDtswm8Y5nFPAdnwekF",
+        .solana: "5gattKnvu5f1NDHBuZ6VfDXjRrJa9UcAArkZ3ys3e82F"
     ]
 
     static var appVersion: String {

piratecash/pcash-wallet-ios

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -1,5 +1,6 @@
 import Foundation
 import UIKit
+import MarketKit
 
 struct AppConfig {
     static let label = "cash.p.terminal"
@@ -19,10 +20,24 @@ struct AppConfig {
     static let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!
     static let faqIndexUrl = URL(string: "https://p.cash/s1/faq.json")!
     static let faqIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/unstoppable-wallet-website/master/src/faq.json")!
-    static let donationAddresses: [String: String] = [
-        "BTC": "3G5fwc9PP9Lcb1y3RAYGzoQZs5enJkmdxN",
-        "ETH": "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
-        "BNB": "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B"
+    static let donationAddresses: [BlockchainType: String] = [
+        .bitcoin: "3G5fwc9PP9Lcb1y3RAYGzoQZs5enJkmdxN",
+        .bitcoinCash: "bitcoincash:qr4f0pkvx86vv6cuae48nj83txqhwyt2fgadd9smxg\n",
+//        .ecash: "ecash:\n",
+        .litecoin: "MNbHsci3A8u6UiqjBMMckXzfPrLjeMxdRC\n",
+        .dash: "XcpUrR8LkohMNB9TfJaC97id6boUhRU3wk",
+        .zcash: "zs1eqk4jh84tas5xv3xydeknm3pvg6cn3l7d2twxh8npcpus6h2gg3dqd8gkxj5zpm98lsj67fkm4f",
+        .ethereum: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+        .binanceSmartChain: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+//        .binanceChain: "",
+        .polygon: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+        .avalanche: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+        .optimism: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+        .arbitrumOne: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+        .gnosis: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+        .fantom: "0x696Ed8f9E2b3265Abc24a6A035d6c5094f61e61B",
+        .tron: "TV4wYRcDun4iHb4oUgcse4Whptk9JKVui2"
+//        .solana: ""
     ]
 
     static var appVersion: String {