3095911332ba18cb3e2087b8a6fd819ffb58a58b
ant013 <ant013@mail.ru>
2023-11-15 14:13:37 +0600

    Handle uri from qr code on main screen

--- UnstoppableWallet/UnstoppableWallet/Modules/Main/Workers/SendAppShowWorker/AddressAppShowModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Main/Workers/SendAppShowWorker/AddressAppShowModule.swift
@@ -1,5 +1,6 @@
 import RxSwift
 import UIKit
+import MarketKit
 
 class AddressAppShowModule {
     private let disposeBag = DisposeBag()
@@ -18,24 +19,39 @@ extension AddressAppShowModule: IEventHandler {
             return
         }
 
-        var uri: String?
-        switch event {
-        case let event as String:
-            uri = event
-        default: ()
-        }
-
-        guard let uri else {
+        guard var address = event as? String else {
             throw EventHandler.HandleError.noSuitableHandler
         }
 
-        let uriParser = AddressParserFactory.parser(blockchainType: <#T##BlockchainType?##MarketKit.BlockchainType?#>)
+        // Handle uri string if exist
+        var uriBlockchainType: BlockchainType? = nil
+        var addressData: AddressData? = nil
+        if AddressUriParser.hasUriPrefix(text: address) {
+            let data = AddressParserFactory.uriBlockchainTypes.map { type -> AddressUriParser in
+                AddressParserFactory.parser(blockchainType: type)
+            }.compactMap { parser in
+                switch parser.parse(paymentAddress: address) {
+                case let .data(addressData):
+                    return (parser.blockchainType, addressData)
+                default: return nil
+                }
+            }.first
 
+            // we can handle one of blockchain types. For .ethereum -> we must return nil, to check all Evm blockchains for now
+            if let data {
+                switch data.0 {
+                case .ethereum: uriBlockchainType = nil
+                default: uriBlockchainType = data.0
+                }
+            }
+            addressData = data?.1
+        }
+        address = addressData?.address ?? address
         let disposeBag = DisposeBag()
-        let chain = AddressParserFactory.parserChain(blockchainType: nil, withEns: false)
+        let chain = AddressParserFactory.parserChain(blockchainType: uriBlockchainType, withEns: false)
         let types = try await withCheckedThrowingContinuation { continuation in
             chain
-                .handlers(address: uri)
+                .handlers(address: address)
                 .subscribe(onSuccess: { items in
                     continuation.resume(returning: items.map { $0.blockchainType })
                 }, onError: { error in
@@ -48,7 +64,9 @@ extension AddressAppShowModule: IEventHandler {
             throw EventHandler.HandleError.noSuitableHandler
         }
 
-        guard let viewController = WalletModule.sendTokenListViewController(allowedBlockchainTypes: types, prefilledAddress: uri) else {
+        guard let viewController = WalletModule.sendTokenListViewController(
+                allowedBlockchainTypes: types,
+                mode: .prefilled(address: address, amount: addressData?.amount.map { Decimal($0) })) else {
             return
         }
 
