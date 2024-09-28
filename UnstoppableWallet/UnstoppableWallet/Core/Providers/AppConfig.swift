horizontalsystems/unstoppable-wallet-ios

Add app token link to main settings page

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -15,6 +15,7 @@ enum AppConfig {
     static let appGitHubRepository = "unstoppable-wallet-ios"
     static let appTwitterAccount = "unstoppablebyhs"
     static let appTelegramAccount = "unstoppable_announcements"
+    static let appTokenTelegramAccount = "BeUnstoppable_bot"
     static let mempoolSpaceUrl = "https://mempool.space"
     static let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!
     static let faqIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/unstoppable-wallet-website/master/src/faq.json")!

piratecash/pcash-wallet-ios

UnstoppableWallet/UnstoppableWallet/Core/Providers/AppConfig.swift
@@ -15,6 +15,7 @@ enum AppConfig {
     static let appGitHubRepository = "pcash-wallet-ios"
     static let appTwitterAccount = "PirateCash_NET"
     static let appTelegramAccount = "piratecash"
+    static let appTokenTelegramAccount = "piratecash_bot"
     static let appRedditAccount = "PirateCash"
     static let mempoolSpaceUrl = "https://mempool.space"
     static let guidesIndexUrl = URL(string: "https://raw.githubusercontent.com/horizontalsystems/blockchain-crypto-guides/v1.2/index.json")!